c60cd6967dc29b680d88767dd051608457c9e08b
micnic <nicu.micleusanu@sendmoments.de>
2013-12-30 16:12:06 +0200

    fixed zooming from second mouse scroll, fixes issue #76

--- modestmaps.js
+++ modestmaps.js
@@ -878,13 +878,12 @@ var MM = com.modestmaps = {
         var handler = { id: 'MouseWheelHandler' },
             map,
             _zoomDiv,
-            prevTime,
+            prevTime = new Date().getTime(),
             precise = false;
 
 
         function mouseWheel(e) {
             var delta = 0;
-            prevTime = prevTime || new Date().getTime();
 
             try {
                 _zoomDiv.scrollTop = 1000;
